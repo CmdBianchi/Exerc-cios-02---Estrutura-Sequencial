956466a9bef0e702c63d397fce396e6511150af8
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-21 09:54:30 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios 02 - Estrutura Sequencial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercícios_02___Estrutura_Sequencial
+{
+    class Program
+    {
+        /// -------> START
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args)
+        {
+            Msg();
+            Formula();
+            
+        }
+        /// -------> FUNCTIONS
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Msg()
+        {
+            Console.WriteLine("Valor da área deste círculo com quatro casas decimais.");
+        }
+        static void Formula()
+        {
+            double area, raio, pi = 3.14159;
+            Console.Write("Digite o valor do RAIO: ");
+            raio = double.Parse(Console.ReadLine());
+            area = pi * raio * raio;
+            Console.Write("O valor da área é: "+area);
+                  
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        /// -------> END
+
+    }
+}
